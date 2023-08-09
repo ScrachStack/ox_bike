bb46aec49c83a4c70f56fd1f40ac277f4f9a45bb
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-09 13:36:34 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+
+Config = {}
+
+Config.BikeSpawnerLocation = {
+    x = -409.20327758789,
+    y = 1171.3475341797,
+    z = 325.8026733984,
+}
+Config.Vehicles = {
+    'zentorno',
+    'adder',
+    -- Add other vehicles here
+}
+
+Config.MaxDistance = 50.0  -- Change this value to whatever distance you'd like
